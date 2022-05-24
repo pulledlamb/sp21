112b0177a718b459ce2d32a20bfbf6c891c9df77
pulledlamb <lojaneve@hotmail.com>
2022-05-24 23:04:12 +1000

    finished guitar hero

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,44 @@
+package gh2;
+
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class GuitarHero {
+    public String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    public static final double CONCERT_A = 440.0;
+    private static final int G = 37;
+    public GuitarString[] gstr = new GuitarString[G];
+
+    public GuitarHero() {
+        for (int i = 0; i < G; i += 1) {
+            double freq = CONCERT_A * Math.pow(2, (i - 24) / 12);
+            gstr[i] = new GuitarString(freq);
+        }
+    }
+
+    public static void main(String[] args) {
+        GuitarHero gh = new GuitarHero();
+
+        while (true) {
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = gh.keyboard.indexOf(key);
+                if (index >= 0) {
+                    gh.gstr[index].pluck();
+                }
+            }
+
+            double sample = 0.0;
+            for (GuitarString gs : gh.gstr) {
+                sample += gs.sample();
+            }
+
+            StdAudio.play(sample);
+
+            for (GuitarString gs : gh.gstr) {
+                gs.tic();
+            }
+        }
+    }
+}
