2728abcbfb6f4757d980b4a6c4c9b0828b81f910
pulledlamb <lojaneve@hotmail.com>
2022-05-25 12:07:19 +1000

    remove tests

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -12,39 +12,35 @@ public class TestArrayDequeEC {
     public void randomizedTest() {
         StudentArrayDeque<Integer> L = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> B = new ArrayDequeSolution<>();
-        ArrayDequeSolution<String> msg = new ArrayDequeSolution<>();
+        StringBuilder msg = new StringBuilder("\n");
 
         int N = 5000;
         for (int i = 0; i < N; i += 1) {
-            int operationNumber = StdRandom.uniform(0, 3);
+            int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
                 L.addLast(randVal);
                 B.addLast(randVal);
-                if (msg.size() > M) {
-                    msg.removeFirst();
-                }
-                msg.addLast("addLast(" + randVal + ")\n");
+                msg.append("addLast(" + randVal + ")\n");
                 //System.out.println("addLast(" + randVal + ")");
             } else if (operationNumber == 1) {
                 // size
                 int size = L.size();
                 //System.out.println("size: " + size);
-                if (msg.size() > M) {
-                    msg.removeFirst();
-                }
-                msg.addLast("size()\n");
+                msg.append("size()\n");
 
                 assertEquals(msg.toString(), L.size(), B.size());
-            } else if (L.size() > 0) {
-                // getLast
-                if (msg.size() > M) {
-                    msg.removeFirst();
-                }
-                msg.addLast("removeLast()\n");
+            } else if (operationNumber == 2 && L.size() > 0) {
+                // removeLast
+                msg.append("removeLast()\n");
 
                 assertEquals(msg.toString(), L.removeLast(), B.removeLast());
+            } else if (L.size() > 0) {
+                //removeFirst
+                msg.append("removeFirst()\n");
+
+                assertEquals(msg.toString(), L.removeFirst(), B.removeFirst());
             }
         }
     }
