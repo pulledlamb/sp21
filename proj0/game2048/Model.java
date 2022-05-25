e1e8f0be5a0882c85e6d44dd6260967a7b863bbd
pulledlamb <lojaneve@hotmail.com>
2022-05-25 10:00:01 +1000

    style check

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -178,8 +178,8 @@ public class Model extends Observable {
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
         int n = b.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++){
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1){
                 if (b.tile(i, j) == null) {
                     return true;
                 }
